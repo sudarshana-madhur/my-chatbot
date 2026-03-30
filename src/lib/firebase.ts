fd5eda01e39db797d8dfdc440bbd97d738006f7b
sudarshana.madhur <sudarshana.madhur@gmail.com>
2026-03-30 10:37:56 +0530

    updated firebase client sdk config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,14 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import FirebaseConfigJson from "../../firebaseConfig.json";
 
-const firebaseConfig = FirebaseConfigJson;
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "my-chatbot-a84b6.firebaseapp.com",
+  projectId: "my-chatbot-a84b6",
+  storageBucket: "my-chatbot-a84b6.firebasestorage.app",
+  messagingSenderId: "502732864962",
+  appId: "1:502732864962:web:fda327e8cf7f3370eaea5b",
+};
 
 // Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
