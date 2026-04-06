d73eb983b278b27da575a6297671f7ca9358387f
sudarshana.madhur <sudarshana.madhur@gmail.com>
2026-04-06 19:14:45 +0530

    updated the models name to be generic so that won't requrie code changes when preview models gets deprecated

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,7 @@
 export const GEMINI_MODELS = [
-  "gemini-3-flash-preview",
-  "gemini-3.1-pro-preview",
-  "gemini-2.5-pro",
-  "gemini-2.5-flash",
+  "gemini-flash-latest",
+  "gemini-flash-lite-latest",
+  "gemini-pro-latest",
 ];
 
 export const systemInstruction = `
